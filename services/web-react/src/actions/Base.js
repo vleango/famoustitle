d519e3dc35556bf785f1d7c6a01b214061c727d7
Tha Leang <vleango@gmail.com>
2018-05-02 07:52:33 -0700

    use correct environments

--- services/web-react/src/actions/Base.js
+++ services/web-react/src/actions/Base.js
@@ -1,1 +1,7 @@
-export const ROOT_API_URL = `http://localhost:4000`
+export let ROOT_API_URL;
+
+if(process.env.NODE_ENV === "production") {
+    ROOT_API_URL = `https://u10yz1nzwc.execute-api.us-west-2.amazonaws.com/production`;
+} else {
+    ROOT_API_URL = `http://localhost:4000`
+}
