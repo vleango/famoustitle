9225a658a6570a67f915968eb412d9db06715c2d
Tha Leang <vleango@gmail.com>
2018-06-19 07:08:25 -0700

    Split CI test for parallelism and to help with dropped db connections

--- services/lambda-go/src/github.com/vleango/test.sh
+++ services/lambda-go/src/github.com/vleango/test.sh
@@ -4,11 +4,19 @@ if [ "$1" != "" ] && [ $1 = "--single" ] || [ "$1" != "" ] && [ $1 = "-s" ]; the
     # sample: testing individual
     go test -v -p 1 -cover github.com/vleango/functions/articles/destroy && echo "All passed"
 else
-    # functions
-    go test -v -p 1 -cover github.com/vleango/functions/... &&
-
-    # lib
-    go test -v -p 1 -cover github.com/vleango/lib/... &&
-
-    echo "All passed"
+    if [ "$APP_ENV" = "ci" ]; then
+        # CI Parallel Tests
+        if [ "$CIRCLE_NODE_INDEX" = 0 ]; then
+            go test -v -p 1 -cover github.com/vleango/functions/... && echo "All passed"
+        else
+            if [ "$CIRCLE_NODE_INDEX" = 1 ]; then
+                go test -v -p 1 -cover github.com/vleango/lib/... && echo "All passed"
+            fi
+        fi
+    else
+        # regular test
+        go test -v -p 1 -cover github.com/vleango/functions/... &&
+        go test -v -p 1 -cover github.com/vleango/lib/... &&
+        echo "All passed"
+    fi
 fi
